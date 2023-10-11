96be1b4016b72ead49d1c58ae824e7c0b5b454a6
kaitona <kaito146remi@gmail.com>
2023-10-12 00:04:21 +0900

    deploy

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -1,7 +1,7 @@
 const chainId = "0x13881"; // (required) chainId to be used
 const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
 
-const quiz_address = "0xF4EDfD8E1946B498c17b15E3E84283E3E38e1a7b";
+const quiz_address = "0x44aB6eedEc8d3f077aa295b1FbfE4Bad1B15282c";
 const token_address = "0x9A108973978Fe5A8ae16f58cbCc92CA2305BdE0E";
 
 export { chainId, rpc, quiz_address, token_address };
