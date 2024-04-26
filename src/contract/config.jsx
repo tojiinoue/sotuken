ad837aec89f2dc5ca65f5766a9902d635d3b00b7
tojiinoue <toji0813@gmail.com>
2024-04-26 11:16:23 +0900

    token.solの書き換え

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -2,6 +2,6 @@ const chainId = "0x13881"; // (required) chainId to be used
 const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
 
 const quiz_address = "0x11d32748A78862e45Eae5026114fF79a39025592";
-const token_address = "0x39ffB57c11187428A09458850D4f64b5a22f6917";
+const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
 
 export { chainId, rpc, quiz_address, token_address };
