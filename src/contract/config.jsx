bf6fbe8488aa06e4046428a5ed6f82b502e5cbdb
tojiinoue <toji0813@gmail.com>
2024-04-29 13:41:16 +0900

    test

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -1,7 +1,7 @@
 const chainId = "0x13881"; // (required) chainId to be used
 const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
 
-const quiz_address = "0x11d32748A78862e45Eae5026114fF79a39025592";
+const quiz_address = "0xB80f73B6be80f39b30bd8624368cDd57E0db3ff5";
 const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
 
 export { chainId, rpc, quiz_address, token_address };
