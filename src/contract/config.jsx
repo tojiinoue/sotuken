0220309bda3aa9d840a7cf235537375d7bf8431b
tojiinoue <toji0813@gmail.com>
2024-05-02 19:56:43 +0900

    test

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -1,7 +1,13 @@
-const chainId = "0x13881"; // (required) chainId to be used
-const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
+//const chainId = "0x13881"; // (required) chainId to be used
+//const rpc = "https://rpc-mumbai.maticvigil.com/"; // (required for Ethereum) JSON RPC endpoint
 
-const quiz_address = "0xB80f73B6be80f39b30bd8624368cDd57E0db3ff5";
-const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
+const chainId = "0x13882"; // (required) chainId to be used
+const rpc = "https://rpc-amoy.polygon.technology/"; // (required for Ethereum) JSON RPC endpoint
+
+//const quiz_address = "0xB80f73B6be80f39b30bd8624368cDd57E0db3ff5";
+//const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
+
+const quiz_address = "0x681913855E68BBF88962A40E4f3f48cB78fc9603";
+const token_address = "0x4147aE2cEC45cFb2B9a9A3aE320C855DD767C2f8";
 
 export { chainId, rpc, quiz_address, token_address };
--- src/contract/network.js
+++ src/contract/network.js
@@ -1,7 +1,7 @@
 export const amoy = {
     id: 80002,
     name: "Amoy",
-    network: "Sepolia",
+    network: "Polygon Pos Amoy",
     iconUrl: "",
     iconBackground: "#000000",
     nativeCurrency: {
