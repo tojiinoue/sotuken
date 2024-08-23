5aa5c9a70b5042f7726f10fb6d9f894364586b24
tojiinoue <toji0813@gmail.com>
2024-08-23 13:08:31 +0900

    tokenアドレス元に戻す

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -7,7 +7,8 @@ const rpc = "https://rpc-amoy.polygon.technology/"; // (required for Ethereum) J
 //const quiz_address = "0xB80f73B6be80f39b30bd8624368cDd57E0db3ff5";
 //const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
 
-const quiz_address = "0x36F15022E960F0F09124FE665C7C6d8F3982aa5e";
+const quiz_address = "0x681913855E68BBF88962A40E4f3f48cB78fc9603";
+//const quiz_address = "0x36F15022E960F0F09124FE665C7C6d8F3982aa5e";
 const token_address = "0x4147aE2cEC45cFb2B9a9A3aE320C855DD767C2f8";
 
 export { chainId, rpc, quiz_address, token_address };
